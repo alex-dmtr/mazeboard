2715625775eb8dbe460eb190ba766597e4bfbda6
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-07-25 09:34:17 +0300

    mend

--- js/main.js
+++ js/main.js
@@ -1,1 +1,58 @@
+const NUM_ROWS = 10;
+const NUM_COLS = 10;
+const EMPTY = 'O';
+const POINT_A = 'A';
+const POINT_B = 'B';
+const OBSTACLE = 'X';
+var matrix = [];
 
+function initMatrix()
+{
+  matrix = [];
+
+  for (let i = 0; i < NUM_ROWS; i++)
+  {
+    matrix[i] = [];
+    for (let j = 0; j < NUM_COLS; j++)
+      matrix[i][j] = EMPTY; 
+  }
+}
+
+function $getMazeElement()
+{
+  return $("#maze");
+}
+function getCellID(row, col)
+{
+  return row*NUM_COLS+col;
+}
+function $getCellElement(row, col)
+{
+  return $('#'+getCellID(row, col));
+}
+function renderMatrix()
+{
+  let $maze = $getMazeElement();
+  $maze.empty();
+  for (let row = 0; row < NUM_ROWS; row++)
+  {
+    $maze.append("<tr>");
+    for (let col = 0; col < NUM_COLS; col++)
+    {
+      $maze.append("<td id="+getCellID(row, col)+">X</td>");
+      let $cell = $getCellElement(row, col);
+      
+      $cell.click(() => {
+        alert(row+","+col);
+      });
+    }
+     
+    $maze.append("</tr>");
+  }
+}
+
+$(document).ready(function() {
+
+  initMatrix();
+  renderMatrix();
+})
